5a19d0e7df57836df43cf99ff7643cc6149edfd1
Matthew Holcombe <matthewholcombe@matthews-mbp-8.myfiosgateway.com>
2022-12-08 11:23:31 -0500

    about me and list of programs

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,8 @@ function Header() {
     <div className='Header_Container'>
       <h1>Matthew Holcombe</h1>
       {/* <h4 className="Job_Title"> Fullstack Software Engineer</h4> */}
-      <p className="Tag_Line"> A fullstack engineer who enjoys bridging the gaps between usability, functionality, and creativity.</p>
+      <h3 className="Tag_Line"> A fullstack engineer who enjoys bridging the gaps between usability, functionality, and creativity.</h3>
+      <p> HTML | CSS | JavaScript | Python | React | MongoDB | SQL |</p>
     </div>
 );
 }
--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -3,14 +3,13 @@ import './Styles/Home.css'
 function Home() {
 
   return (
-    <>
-      <h1>Matthew Holcombe</h1>
-      <p className='Home_text'> Illuimating the web with usability, functionality, and creativity </p>
+    <div className="Bio_content">
+      <p className='Home_text'> Hello, and thank you for visiting my site. I'm Matt a full stack software engineer with aspirations to become a project manager. I use my ten years as a theatre desgiener and educator, as well as my vastily growing software experience to make the web a more beautiful and interactive place. Illuimating the web with usability, functionality, and creativity there is nothing we cannot achieve.  </p>
+      <break></break>
+      <p className='Home_text'> Through my site you will find a list of projects, my resume, more about me, contact information, and  maybe even a suprise or two.   </p>
 
 
-
-
-    </>
+    </div>
 
 
 );
